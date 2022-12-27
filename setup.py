06472ce7e265933b9cc47f1af4a3e3571bd71202
Abreto Fu <hi@abreto.email>
2022-12-27 08:27:39 +0800

    Add install deps

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import find_packages, setup
 
+DEPS = ["PyYAML"]
+
 setup(
     name="reflinkcep",
     version="0.1",
     author="Xuandeng Fu",
     author_email="fuxd@ios.ac.cn",
     packages=find_packages(),
+    install_requires=DEPS,
 )
